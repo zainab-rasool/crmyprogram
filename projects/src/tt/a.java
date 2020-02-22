e0ba9c1938ba3fddee998bbcfef6c9d3cb742731
Honey <Honey@Honey-PC>
2020-02-22 11:40:13 +0530

    i am pushing my code

---
+++ projects/src/tt/a.java
@@ -0,0 +1,29 @@
+package tt;
+
+import java.util.Scanner;
+
+
+
+public class a {
+	
+	public void tableprint(int n)
+	{
+		
+	System.out.println("Table is = ");
+     for(int i=1; i<=10; i++)
+     {
+    	 int t=n*i;
+    	 System.out.println("" +t);
+     }
+	}
+
+	
+	public static void main(String[] args){
+		a obj=new a();
+		Scanner s=new Scanner(System.in);
+		System.out.println("Enter the number = ");
+		int n=s.nextInt();
+		obj.tableprint(n);
+	}
+
+}
